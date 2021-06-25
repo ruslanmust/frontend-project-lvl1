ed05ae0f9625f1d1ce4c9dd0f4b4548e03f6514f
Rozhkov R.R <rozhkov.r.r@sberbank.ru>
2021-06-25 09:17:32 +0300

    run prettier

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const whatIsName = () => {
-  const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question("May I have your name? ");
 
   console.log(`Hello, ${name}!`);
 };
