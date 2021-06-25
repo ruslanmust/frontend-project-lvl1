14fbb99659af8c99236247809e23623beb817f97
Rozhkov R.R <rozhkov.r.r@sberbank.ru>
2021-06-25 09:13:33 +0300

    add dir src and file cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const whatIsName = () => {
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+};
+
+export default whatIsName;
